25dab7c7c2fcbf8996a77da3fa5e387e015d8821
subi.prabhakaran <soorya2508@gmail.com>
2014-05-08 18:17:22 +0530

    random

---
+++ random.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main ()
+{
+    int i, input;
+    
+    srand ( time(NULL) );
+    /* initialize random seed: */
+    for(i=0;i<10;i++){
+
+    /* Generate a random number: */
+    int number = rand() % 255 ;
+    printf("\n %d \n", number);
+    }
+
+    return 0;
+}
